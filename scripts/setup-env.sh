7140ea1cc003e1b845031c0fda0b6c1058c61c2f
xiccin <su@xicc.in>
2022-06-10 08:54:51 +0530

    modified:   container-setup/docker-compose.yml 	modified:   data/csgo/cfg/sourcemod/gokz/options.cfg 	modified:   data/csgo/cfg/sourcemod/movementhud-defaults.cfg 	modified:   scripts/setup-env.sh

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -10,5 +10,5 @@ apt-get upgrade -y &>> /root/scripts/logs/setup.log
 
 echo -e "[*] installing tools and deps"
 apt-get install locales procps vim ack screen net-tools file wget curl tar p7zip-full bzip2 rsync -y &>> /root/scripts/logs/setup.log
-apt-get install libstdc++6:i386 libgcc1:i386 libc6:i386 libstdc++6:i386 zlib1g:i386 libncurses5:i386 libstdc++6 libgcc1 libc6 libstdc++6 zlib1g libncurses5 -y &>>/root/scripts/logs/setup.log
+apt-get install libstdc++6:i386 libgcc1:i386 libc6:i386 libstdc++6:i386 zlib1g:i386 libcurl4:i386 libncurses5:i386 libstdc++6 libgcc1 libc6 libstdc++6 zlib1g libcurl4 libncurses5 -y &>>/root/scripts/logs/setup.log
 localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8 &>> /root/scripts/logs/setup.log
